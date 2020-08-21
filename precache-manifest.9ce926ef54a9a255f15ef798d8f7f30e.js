45f442d3932c08e4eb3e90e07e3424f19865c1f9
Jayden Chan <jaydencn7@gmail.com>
2020-08-21 05:04:25 -0600

    Updates

---
+++ precache-manifest.9ce926ef54a9a255f15ef798d8f7f30e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237de0a4a7131bc587f417514abe893a",
+    "url": "/valorant-scrim-logs/index.html"
+  },
+  {
+    "revision": "11291dceaf7e9d5c88d8",
+    "url": "/valorant-scrim-logs/static/css/main.34cf7757.chunk.css"
+  },
+  {
+    "revision": "72632a832fc389572574",
+    "url": "/valorant-scrim-logs/static/js/2.e412e31f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/valorant-scrim-logs/static/js/2.e412e31f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11291dceaf7e9d5c88d8",
+    "url": "/valorant-scrim-logs/static/js/main.c80917f1.chunk.js"
+  },
+  {
+    "revision": "641f71834d2dae96a379",
+    "url": "/valorant-scrim-logs/static/js/runtime-main.f3ede612.js"
+  }
+]);
